ca3564b08e27047ce77ae5e618f754fb8ee5a515
EnricoLi <enricolima.eng@gmail.com>
2022-09-11 11:06:37 -0300

    vars

--- client/src/components/shortInput.js
+++ client/src/components/shortInput.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import "./shortInput.css"
 
-const HOST = process.env.HOST;
+const HOST = process.env.HOST || "https://shlz.herokuapp.com";
 
 export const ShortInput = (props) => {
     const [input, setInput] = useState(props?.value ?? '');
